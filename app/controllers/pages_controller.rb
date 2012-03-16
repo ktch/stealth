bc2e49cc2133f3ac1f0b54bc2e917a60225d96e0
Zach Phillips <zach@thekitchen.co>
2012-03-16 08:39:31 -0400

    First commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  
+  def keynote
+    
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SthealthSpec::Application.config.secret_token = 'a4476b7106672e292f5c74bcddfacbbfa22d4b12b4015b51031390475894fe0527e8997b2b68964178f609e48843acf5d24873e0646611eefbf81bff1b651539'
